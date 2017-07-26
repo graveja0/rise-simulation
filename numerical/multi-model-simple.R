8dad34583d5b9ca276cf21a97be7a2f0790d05d8
Shawn Garbett <Shawn.P.Garbett@Vanderbilt.edu>
2017-07-26 11:40:10 -0500

    First draft of simple numerical costs/QALY

--- numerical/multi-model-simple.R
+++ numerical/multi-model-simple.R
@@ -28,17 +28,29 @@ params <- c(
   rr_b2  = 0.5,                # Relative Risk for Event B with better treatment informed by genotyping
   
   # Costs
-  c_a   = 10000,              # Cost of event A
-  c_bs  = 25000,              # Cost for event B survival
-  c_bd  = 15000,              # Cost for event B death
-  c_tx  = 100/30,             # Daily cost of standard treatment
-  c_alt = 300/30,             # Daily cots of alternate treatment
-  c_t   = 100,                # Cost of test
+  c_a1   = 10000,              # Cost of event A
+  c_bs1  = 25000,              # Cost for event B survival
+  c_bd1  = 15000,              # Cost for event B death
+  c_tx1  = 100/30,             # Daily cost of standard treatment
+  c_alt1 = 300/30,             # Daily cots of alternate treatment
 
+  
+  c_a2   = 10000,              # Cost of event A
+  c_bs2  = 25000,              # Cost for event B survival
+  c_bd2  = 15000,              # Cost for event B death
+  c_tx2  = 100/30,             # Daily cost of standard treatment
+  c_alt2 = 700/30,             # Daily cots of alternate treatment
+
+  c_t    = 100,                # Cost of test
+  
   # Disutility
-  d_a   = 0.2,                # Disutility for event A
-  d_at  = 3*365,              # Time in days for disutility of event A
-  d_b   = 0.15,               # Permanent Disutility for event B
+  d_a1   = 0.2,                # Disutility for event A
+  d_at1  = 3*365,              # Time in days for disutility of event A
+  d_b1   = 0.15,               # Permanent Disutility for event B
+  
+  d_a2   = 0.2,                # Disutility for event A
+  d_at2  = 3*365,              # Time in days for disutility of event A
+  d_b2   = 0.15,               # Permanent Disutility for event B
   
 
   disc_rate = 1e-12           # For computing discount
@@ -57,37 +69,6 @@ points(sim_adj_age, f_40yr_percent_d)
 f_40yr_drate <- function(t) inst_rate(pmin(f_40yr_per_d_spline(t), 1),1)
 curve(f_40yr_drate, from=0, to=90)
 
-####################################
-# Integrations of death rates for exposure calculations in delay
-# Now, a special function used in delay equation (Had to put upper bound at 81)
-# F_40yr_drate_5yr <- Vectorize(function(t)
-# {
-#   integrate(f_40yr_drate, lower=max(t-5, 0), upper=min(t, 81))$value
-# })
-# 
-# 
-# F_40yr_drate_1yr <- Vectorize(function(t)
-# {
-#   integrate(f_40yr_drate, lower=max(t-1, 0), upper=min(t, 81))$value
-# })
-
-
-# Does a spline work faster?
-
-# x <- 0:160 / 2
-# y <- exp(-5*params['r_b'] - F_40yr_drate_5yr(x))
-# plot(x, y, typ='l')
-# f <- splinefun(x, y)
-# curve(f, add=TRUE, col='red', lty=2)
-# F_40yr_drate_5yr <- f
-# 
-# 
-# x <- 0:160 / 2
-# y <- exp(-F_40yr_drate_1yr(x))
-# plot(x, y, typ='l')
-# f <- splinefun(x, y)
-# curve(f, add=TRUE, col='red', lty=2)
-# F_40yr_drate_1yr <- f
 
 # This is for doing numberical integration of a set of numbers at an even interval
 alt_simp_coef <- function(i)
@@ -131,7 +112,7 @@ Multi <- function(t, y, params)
       b_n1  =                                                          +r_b1*(1-p_bd1)*a_p1 +r_b1*rr_b1*(1-p_bd1)*a_a1 -r_d1*b_n1,
       b_d1  =                                                          +r_b1*p_bd1*a_p1     +r_b1*rr_b1*p_bd1*a_a1,
       a_c1  =            +r_a1*h_u1             +r_a1*h_t1,
-      b_c1  =                                                          +r_b1*a_p1           +r_b1*rr_b1*a_a1,
+      b_c1  =                                                          +r_b1*(1-p_bd1)*a_p1 +r_b1*rr_b1*(1-p_bd1)*a_a1,
         
       h_u2  = -r_p2*h_u2 -r_a2*h_u2                                                                                    -r_d2*h_u1,
       h_t2  = +r_p2*h_u2 -r_a2*h_t2                                                                                    -r_d2*h_t1,
@@ -142,81 +123,89 @@ Multi <- function(t, y, params)
       a_c2  =            +r_a2*h_u2             +r_a2*h_t2,
       b_c2  =                                                          +r_b2*a_p2           +r_b2*rr_b2*a_a2,
       
+      test  = +p_o*(r_a1*h_u1 + r_a2*h_u2),
       disc  = -disc_rate*disc            # Simple discount rate
     ))
   })
 }
 
-yinit <- rep(0, 17)
-names(yinit) <- c("h_u1", "h_t1", "a_p1", "a_a1", "b_n1", "b_d1", "a_c1", "b_c1", "h_u2", "h_t2", "a_p2", "a_a2", "b_n2", "b_d2", "a_c2", "b_c2", "disc")
+yinit <- rep(0, 18)
+names(yinit) <- c("h_u1", "h_t1", "a_p1", "a_a1", "b_n1", "b_d1", "a_c1", "b_c1",
+                  "h_u2", "h_t2", "a_p2", "a_a2", "b_n2", "b_d2", "a_c2", "b_c2",
+                  "test", "disc")
 yinit[1]  <- 1
 yinit[9]  <- 1
-yinit[17] <- 1
+yinit["disc"] <- 1
 
 times <- seq(0, 40, by=1/365)  # units of years, increments of days, everyone dies after 120, so simulation is cut short
 print(system.time(out <- ode(yinit, times, Multi, params)))
 
 plot(out)
 
+# Check that living population totals are in agreement within numerical error
 l1 <- out[,'h_u1'] + out[,'h_t1'] + out[,'a_p1'] + out[,'a_a1'] + out[,'b_n1']
 l2 <- out[,'h_u2'] + out[,'h_t2'] + out[,'a_p2'] + out[,'a_a2'] + out[,'b_n2']
-
-plot(l1-l2, typ='l', main="Abs Total Numerical Error")
-
+#plot(l1-l2, typ='l', main="Abs Total Numerical Error")
 cat("Consistent population?", all(abs(l1-l2)<1e-8), '\n')
 
-stop("Working Halt")
+
 
 costs <- function(solution, params)
 {
   n <- length(solution[,1])
   simpson <- alt_simp_coef(n)
-
+  step     <- solution[2,'time'] - solution[1,'time']
+  
   with(as.list(params), {
     # Compute Discounted Cost
-    cost <- c_a*sum(diff(solution[,'a'])*solution[2:n,'disc']) + # Cost * Number of events in bucket a
-            c_b*sum(diff(solution[,'b'])*solution[2:n,'disc']) + # Cost * Number of events in bucket b
-            c_t*solution[1, 'h']  # Therapy Cost * Initial healthy individuals
-    
-    # Step size of simulation
-    step     <- solution[2,'time'] - solution[1,'time']
-    
+    cost <- c_a1  *sum(diff(solution[,'a_c1'])*solution[2:n,'disc']) +
+            c_bs1 *sum(diff(solution[,'b_c1'])*solution[2:n,'disc']) +
+            c_bd1 *sum(diff(solution[,'b_d1'])*solution[2:n,'disc']) +
+            c_tx1 *365*sum(simpson*solution[,'a_p1']*solution[,'disc'])*step +
+            c_alt1*365*sum(simpson*solution[,'a_a1']*solution[,'disc'])*step +
+            c_a2  *sum(diff(solution[,'a_c2'])*solution[2:n,'disc']) +
+            c_bs2 *sum(diff(solution[,'b_c2'])*solution[2:n,'disc']) +
+            c_bd2 *sum(diff(solution[,'b_d2'])*solution[2:n,'disc']) +
+            c_tx2 *365*sum(simpson*solution[,'a_p2']*solution[,'disc'])*step +
+            c_alt2*365*sum(simpson*solution[,'a_a1']*solution[,'disc'])*step +
+            c_t   *sum(diff(solution[,'test'])*solution[2:n,'disc'])
+
     # Total possible life units is integral of discounted time
     life <- sum(simpson*solution[,'disc'])*step
     
-    # Permanent disutility for A (integration)
-    disA <- d_a*sum(simpson*solution[,'e10']*solution[,'disc'])*step + 
-            d_a*sum(simpson*solution[,'e15']*solution[,'disc'])*step +
-            d_a*sum(simpson*solution[,'e2' ]*solution[,'disc'])*step
+    disA <- NA # Need to figure out
     
-    # Event B
-    disB <- d_b*sum(simpson*solution[,'db']*solution[,'disc'])*step
+    # Permanent disutility for B (integration)
+    disB <- d_b1*sum(simpson*solution[,'b_n1']*solution[,'disc'])*step + 
+            d_b2*sum(simpson*solution[,'b_n2']*solution[,'disc'])*step
+      
+    # Total living in model
+    life <- solution[,'h_u1'] +
+            solution[,'h_t1'] +
+            solution[,'a_p1'] +
+            solution[,'a_a1'] +
+            solution[,'b_n1']
+    pQALY <- sum(simpson*life*solution[,'disc'])*step
 
-    # Death disutility
-    disD <- sum(simpson*solution[,'d']*solution[,'disc'])*step       # Death disutility (integration)
-    
     c(cost       = unname(cost),
-      qaly       = unname(life-disA-disB-disD),
-      possible   = unname(life),
-      disutility = unname(disA+disB+disD),
-      a_count    = unname(solution[n,'a']),
+      qaly       = unname(pQALY-disA-disB),
+      possible   = unname(pQALY),
       disutil_a  = unname(disA),
-      b_count    = unname(solution[n,'b']),
       disutil_b  = unname(disB),
-      dead_count = unname(solution[n,'d']), 
-      disutil_d  = unname(disD),
-      living     = unname(solution[n,'h']+solution[n,'e10']+solution[n,'e15']+solution[n,'e2'])
+      a1_count   = unname(solution[n,'a_c1']),
+      b1_count   = unname(solution[n,'b_n1']+solution[n,'b_d1']),
+      living     = unname(life[n])
       )
   })
 }
 
-expected <- function(params) costs(dede(yinit, times, Simple, params), params)
+expected <- function(params) costs(ode(yinit, times, Multi, params), params)
 
 round(expected(params), 4)
-
-params['r_a'] <- params['r_a']*0.5
-params['c_t']  <- 2000
-round(expected(params), 4)
+stop("Working Halt")
+# params['r_a'] <- params['r_a']*0.5
+# params['c_t']  <- 2000
+# round(expected(params), 4)
 
 # Some diagnostic plots
 # 
