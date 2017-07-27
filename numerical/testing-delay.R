a8395232a7a4b793cdf50c9b7c9d3b2dda9da53c
Shawn Garbett <Shawn.P.Garbett@Vanderbilt.edu>
2017-07-27 14:33:13 -0500

    Found alternate method of integrating delay differential losses (YEAH)

--- numerical/testing-delay.R
+++ numerical/testing-delay.R
@@ -23,6 +23,7 @@ DelayTest <- function(t, y, params)
 {
   with(as.list(c(y, params)), {
     finished <- if(t < 2) 0 else lagderiv(t-2, 2)*exp(-G(t, params))
+    finished_alt <- if(t < 2) 0 else lagderiv(t-2, 2)*exp(-altG)
     
     list(
       c(
@@ -30,15 +31,36 @@ DelayTest <- function(t, y, params)
         eventA   = f(t, params)*healthy,
         experA   = f(t, params)*healthy -g(t, params)*experA - finished,
         postA    = finished - g(t, params)*postA,
-        deaths   = g(t, params)*(healthy+experA+postA)
-      )
+        deaths   = g(t, params)*(healthy+experA+postA),
+        
+        # Alternate method of determining integral G when unavailable
+        accG     = g(t, params),  # Accumulator
+        altG     = g(t, params) - if(t < 2) 0 else lagderiv(t-2, 6), # Lagged Interval
+        
+        # Demonstration that it works.
+        experA_alt   = f(t, params)*healthy -g(t, params)*experA - finished_alt
+      ),
+      G = G(t, params)
     )
   })
 }
 
-yinit <- c(healthy=1, eventA=0, experA=0, postA=0, deaths=0)
+yinit <- c(healthy=1, eventA=0, experA=0, postA=0, deaths=0, accG=0, altG=0, experA_alt=0)
 
 times <- seq(0, 10, by=1/20)  # units of years, increments of days, everyone dies after 120, so simulation is cut short
 print(system.time(out <- dede(yinit, times, DelayTest, params)))
+
 plot(100*out)
-plot(1 - out[,'healthy'] - out[,'deaths'] - out[,'experA'] - out[,'postA'], typ='l', ylab="Error")
+#plot(1 - out[,'healthy'] - out[,'deaths'] - out[,'experA'] - out[,'postA'], typ='l', ylab="Error")
+
+pp <- function(data, vars)
+{
+  colors <- rainbow_hcl(length(vars))
+  plot(data[,'time'], data[,vars[1]], typ='l', ylab='', xlab="time", col=colors[1])
+  for(i in 2:length(vars))
+  {
+    lines(data[,'time'], data[,vars[i]], col=colors[i])
+  }
+}
+
+#pp(100*out, c("altG", "G"))
