6ebd8f8f0e79198b915cd5100fb25522c0831163
graveja0 <john.graves@vanderbilt.edu>
2017-08-25 19:59:36 -0500

    accre file fix

--- nber-aug2017/run-nber-psa.R
+++ nber-aug2017/run-nber-psa.R
@@ -5,9 +5,194 @@ invisible(lapply(pkg, require, character.only = TRUE))
 run.id.base <- "vogi-numerical"
 Scenarios <- list (c("None","Single"),c("None","None"),c("None","Panel"),c("Panel","None"))
 
-mainDir <- "../run-data"
+mainDir <- "./run-data"
 subDir <- run.id.base
 
 if (!file.exists(file.path(mainDir,subDir))){
     dir.create(file.path(mainDir, subDir))
   }
+
+scenario.file ="./simple-pgx-scenario-parameters-vogi.csv"
+
+# Read in the scenario spreadsheet and map the (long) scenario names to a generic A, B, C, etc.
+scenarios <- read.csv(scenario.file,stringsAsFactors = FALSE) %>% tbl_df(); head(scenarios)
+scenario.names <- scenarios %>% dplyr::select(-param,-type,-value,-psatype,-description,-dplyr::contains("psa_param")) %>% names()
+
+# allow for up to 720 scenarios (just need enough scenario IDs)
+lots.of.letters <- c(LETTERS, sapply(LETTERS, function(x) paste0(x, LETTERS)))
+scenario.ids <- paste0("SC_",lots.of.letters[1:length(scenario.names)])
+scenario.mapping <- cbind.data.frame(scenario.id = scenario.ids,scenarnio.name = scenario.names)
+scenario.mapping <- scenario.names
+names(scenario.names) = gsub("SC_","",scenario.ids)
+
+require(lhs)
+
+draw.latin.hypercube <- function(tt, PSA.N = 10) {
+  params.full <-
+    scenarios %>% filter(type == tt) %>% select(-value, -description)
+  names.temp <- names(params.full)
+  for (y in scenario.names)
+    names.temp <-
+    gsub(paste0(paste0("^", y, "_"), "|", paste0("^", y, "$")),
+         paste0("SC_", names(scenario.names[which(scenario.names == y)])),
+         names.temp)
+  names(params.full) <- gsub("psa_param", "_psa", names.temp)
+  if (tt != "global")
+  {
+    params.full %>% reshape2::melt(id.vars = c("param", "psatype", "type")) %>% tbl_df() %>%
+      mutate(paramtype = gsub(paste0(scenario.ids, collapse = "|"), "", variable)) %>%
+      mutate(
+        variable = gsub("_psa1|_psa2", "", variable) ,
+        paramtype = gsub("^_", "", paramtype)
+      ) %>%
+      mutate(paramtype = ifelse(paramtype == "", "value", paramtype)) %>%
+      mutate(paramtype = ifelse(
+        psatype == "uniform",
+        gsub("psa1", "min", paramtype),
+        paramtype
+      )) %>%
+      mutate(paramtype = ifelse(
+        psatype == "uniform",
+        gsub("psa2", "max", paramtype),
+        paramtype
+      )) %>%
+      mutate(paramtype = ifelse(
+        psatype == "beta",
+        gsub("psa1", "shape1", paramtype),
+        paramtype
+      )) %>%
+      mutate(paramtype = ifelse(
+        psatype == "beta",
+        gsub("psa2", "shape2", paramtype),
+        paramtype
+      )) %>%
+      mutate(paramtype = ifelse(
+        psatype == "constant",
+        gsub("psa1", "constant1", paramtype),
+        paramtype
+      )) %>%
+      mutate(paramtype = ifelse(
+        psatype == "constant",
+        gsub("psa2", "constant2", paramtype),
+        paramtype
+      )) %>%
+      rename(scenario = variable) %>% reshape2::dcast(param + psatype +
+                                                        type + scenario ~ paramtype) %>% data.frame() %>%
+      tidyr::unite(parameter, param, scenario) -> params2
+  } else
+  {
+    params.full %>% reshape2::melt(id.vars = c("param", "psatype", "type")) %>% mutate(paramtype = gsub(paste0(scenario.ids, collapse =
+                                                                                                                 "|"), "", variable)) %>%
+      mutate(
+        variable = gsub("_psa1|_psa2", "", variable) ,
+        paramtype = gsub("^_", "", paramtype)
+      ) %>%
+      filter(variable == "SC_A") %>%
+      mutate(paramtype = ifelse(paramtype == "", "value", paramtype)) %>%
+      mutate(paramtype = ifelse(
+        psatype == "uniform",
+        gsub("psa1", "min", paramtype),
+        paramtype
+      )) %>%
+      mutate(paramtype = ifelse(
+        psatype == "uniform",
+        gsub("psa2", "max", paramtype),
+        paramtype
+      )) %>%
+      mutate(paramtype = ifelse(
+        psatype == "beta",
+        gsub("psa1", "shape1", paramtype),
+        paramtype
+      )) %>%
+      mutate(paramtype = ifelse(
+        psatype == "beta",
+        gsub("psa2", "shape2", paramtype),
+        paramtype
+      )) %>%
+      mutate(paramtype = ifelse(
+        psatype == "constant",
+        gsub("psa1", "constant1", paramtype),
+        paramtype
+      )) %>%
+      mutate(paramtype = ifelse(
+        psatype == "constant",
+        gsub("psa2", "constant2", paramtype),
+        paramtype
+      )) %>%
+      rename(scenario = variable) %>% reshape2::dcast(param + type + psatype + scenario ~
+                                                        paramtype) %>%
+      mutate(scenario = "global")  %>% rename(parameter = param) -> params2
+  }
+  params.as.list <-
+    setNames(split(params2, 1:nrow(params2)), params2$parameter) %>% purrr::map( ~
+                                                                                   as.list(.x))
+  params <- unlist(lapply(params.as.list, function(x)
+    x$value))
+  
+  X <- randomLHS(vN_PSA, length(params))
+  colnames(X) = names(params)
+  
+  lhc.draws.transformed <-
+    cbind.data.frame(lapply(params.as.list, function(x)
+    {
+      if (x[["psatype"]] == "beta")
+      {
+        qbeta(X[, x[["parameter"]]], shape1 = x[["shape1"]], shape2 = x[["shape2"]])
+      }
+      else if (x[["psatype"]] == "uniform")
+      {
+        qunif(X[, x[["parameter"]]], min = x[["min"]], max = x[["max"]])
+      }
+      else if (x[["psatype"]] == "constant")
+      {
+        rep(x[["value"]], PSA.N)
+      }
+    }))
+  lhc.draws.transformed
+}
+
+drawn.parameter.values <- unique(scenarios$type) %>% purrr::map(~draw.latin.hypercube(tt=.x,PSA.N=vN_PSA) )
+names(drawn.parameter.values) <- unique(scenarios$type)
+
+
+ss_death <- read.csv("./numerical/ss-death-2011.csv")
+
+source("./numerical/model-n-simple.R")
+
+config   <- drawn.parameter.values
+times    <- seq(0, 80, by=2/365)
+scenario <- "none"
+
+library(doParallel)
+library(broom)
+
+n.cores <- parallel::detectCores()
+for (ss in c("none","reactive-single","reactive-panel","preemptive-panel"))
+{
+  registerDoParallel(cores = n.cores)
+  getDoParWorkers()
+  
+  d <- foreach(ii = seq(vN_PSA), .combine = rbind) %dopar%
+  {
+    library(deSolve)
+    library(dplyr)
+    model.run(config, ii, ss, times)
+  }
+  
+  temp <-
+    d %>% tbl_df() %>% cbind(as.data.frame(drawn.parameter.values))
+  varying1 <-
+    suppressWarnings(temp %>% map_dbl( ~ var(., na.rm = TRUE)))
+  varying <- unique(c("dQALY","dCOST",names(varying1[which(varying1 > 0)])))
+  temp <-
+    temp[, varying] %>% data.frame() %>% dplyr::mutate(iteration =
+                                                         row_number())  %>%
+    dplyr::select(iteration,
+                  dplyr::contains("QALY"),
+                  dplyr::contains("COST"),
+                  everything()) %>% mutate(strategy = ss)
+  assign(paste0("results.", ss), temp)
+}
+
+
+
