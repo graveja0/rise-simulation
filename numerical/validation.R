8167083fdab30fea1a874c0396687e37ac5d72a5
Shawn Garbett <Shawn.P.Garbett@Vanderbilt.edu>
2017-08-29 12:08:59 -0500

    Got rid of more scaling in validation plots

--- numerical/validation.R
+++ numerical/validation.R
@@ -44,9 +44,12 @@ n <- length(z$start_time)
 plot(z$start_time/365, 1:n/2e5, typ="l", main="Event B2 Compared", ylab="population frac", xlab="years")
 lines(x[,1], (x[,maps('b_c',2)]+x[,maps('b_d',2)] ) , col='red', lty=2)
 
-
 dev.off()
+
+
 # B Death Rates compared
 z <- subset(y, resource %in% c("B_Death_SC_A", "B_Death_SC_B"))
-plot(ecdf(z$start_time/365), main="Event B Death Compared", xlab="cumulative", ylab="years")
-lines(x[,1], (x[,maps('b_d',1)]+x[,maps('b_d',2)] ) * 2e5 / length(unique(z$name)) , col='red', lty=2)
+n <- length(z$start_time)
+
+plot(z$start_time/365, 1:n/2e5, typ="l",main="Event B Death Compared", xlab="population frac", ylab="years")
+lines(x[,1], (x[,maps('b_d',1)]+x[,maps('b_d',2)] ), col='red', lty=2)
