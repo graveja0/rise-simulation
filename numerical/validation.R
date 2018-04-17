2dc10a98888cc2a64c0377f84b3fcb4b16d6b603
Shawn Garbett <shawn.garbett@vanderbilt.edu>
2018-04-17 09:54:07 -0500

    Validation figure modifications for John / Paper

--- numerical/validation.R
+++ numerical/validation.R
@@ -16,40 +16,54 @@ x        <- dede(init, times, genModel, params)
 round(costs(x, params), 3)
 round(costs(x, params)*2e5, 2)
 
-par(mfrow=c(2,2))
+#par(mfrow=c(2,2))
 # Incidence of A_SC_A compared
+postscript("Validation-Fig-A.eps", horizontal=TRUE, width=6, height=6)
 z <- subset(y, resource == "A_SC_A")
 n <- length(z$start_time)
-plot(z$start_time/365, 1:n/2e5, typ="l", main="Event A1 Compared", ylab="population frac", xlab="years")
-lines(x[,1], x[,maps('a_c',1)], col='red', lty=2)
-legend(30, 0.2, c("Delay Diff Eq", "Simulation"), lty=c(2,1), col=c("red", "black"))
+
+points <- seq(1, n, by=1000)
+plot(z$start_time[points]/365, (1:n)[points]/2e5, main="Event A1 Compared", ylab="population frac", xlab="years")
+lines(x[,1], x[,maps('a_c',1)], col='black', lty=2)
+legend(30, 0.2, c("Delay Diff Eq", "Simulation"), lty=c(2,NA), pch=c(NA, 1), col=c("black", "black"))
+dev.off()
 
 # Incidence of A_SC_B compared
+postscript("Validation-Fig-B.eps", horizontal=TRUE, width=6, height=6)
 z <- subset(y, resource == "A_SC_B")
 n <- length(z$start_time)
-plot(z$start_time/365, (1:n)/2e5, typ="l", main="Event A2 Compared", ylab="population frac", xlab="years")
-lines(x[,1], x[,maps('a_c',2)], col='red', lty=2)
+points <- seq(1, n, by=1000)
+plot(z$start_time[points]/365, (1:n)[points]/2e5, main="Event A2 Compared", ylab="population frac", xlab="years")
+lines(x[,1], x[,maps('a_c',2)], lty=2)
+dev.off()
 
 
 # Incidence of B_SC_A compared
+postscript("Validation-Fig-C.eps", horizontal=TRUE, width=6, height=6)
 z <- subset(y, resource == "B_SC_A")
 n <- length(z$start_time)
-plot(z$start_time/365, 1:n/2e5, typ="l", main="Event B1 Compared", ylab="population frac", xlab="years")
-lines(x[,1], (x[,maps('b_c',1)]+x[,maps('b_d',1)]) , col='red', lty=2)
+points <- seq(1, n, by=500)
+plot(z$start_time[points]/365, (1:n)[points]/2e5, main="Event B1 Compared", ylab="population frac", xlab="years")
+lines(x[,1], (x[,maps('b_c',1)]+x[,maps('b_d',1)]), lty=2)
+dev.off()
 
 
 # Incidence of B_SC_B compared
+postscript("Validation-Fig-D.eps", horizontal=TRUE, width=6, height=6)
 z <- subset(y, resource == "B_SC_B")
 n <- length(z$start_time)
-plot(z$start_time/365, 1:n/2e5, typ="l", main="Event B2 Compared", ylab="population frac", xlab="years")
-lines(x[,1], (x[,maps('b_c',2)]+x[,maps('b_d',2)] ) , col='red', lty=2)
-
+points <- seq(1, n, by=750)
+plot(z$start_time[points]/365, (1:n)[points]/2e5, main="Event B2 Compared", ylab="population frac", xlab="years")
+lines(x[,1], (x[,maps('b_c',2)]+x[,maps('b_d',2)] ), lty=2)
 dev.off()
 
 
 # B Death Rates compared
+postscript("Validation-Fig-E.eps", horizontal=TRUE, width=6, height=6)
 z <- subset(y, resource %in% c("B_Death_SC_A", "B_Death_SC_B"))
 n <- length(z$start_time)
+points <- seq(1, n, by=50)
 
-plot(z$start_time/365, 1:n/2e5, typ="l",main="Event B Death Compared", xlab="population frac", ylab="years")
-lines(x[,1], (x[,maps('b_d',1)]+x[,maps('b_d',2)] ), col='red', lty=2)
+plot(z$start_time[points]/365, (1:n)[points]/2e5, main="Event B Death Compared", xlab="population frac", ylab="years")
+lines(x[,1], (x[,maps('b_d',1)]+x[,maps('b_d',2)] ), lty=2)
+dev.off()
